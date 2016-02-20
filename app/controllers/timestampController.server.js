fec347026dcf73737f3884a9e4974ce41fb18122
James <phyoe.wai@me.com>
2016-02-20 18:46:55 +0000

    Fix regex

--- app/controllers/timestampController.server.js
+++ app/controllers/timestampController.server.js
@@ -7,8 +7,8 @@ function TimeStampConverter() {
 
     this.convert = function(req, res) {
         var time = req.params.time;
-        var isNumRegex = /\d+/;
         var unix, natural;
+        var isNumRegex = /^\d+$/;
         if (isNumRegex.test(time)) {
             natural = moment.unix(time).format("MMMM D, YYYY");;
             unix = time;
